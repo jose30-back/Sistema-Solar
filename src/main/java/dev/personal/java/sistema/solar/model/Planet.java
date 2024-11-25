c2f963d61f25b6328640aef5d4eb9fdfc27f6587
jose30-back <josema.bootcamp@gmail.com>
2024-11-25 19:11:23 +0100

    two attributes added, modified the constructor and getters

--- src/main/java/dev/personal/java/sistema/solar/model/Planet.java
+++ src/main/java/dev/personal/java/sistema/solar/model/Planet.java
@@ -1,5 +1,84 @@
 package dev.personal.java.sistema.solar.model;
 
-public class Planets {
+public class Planet {
 
+    String name = null;
+    int numberOfSatellites = 0;
+    double mass = 0;
+    double volume = 0;
+    int diameter = 0;
+    int distanceToTheSun = 0;
+    TypeOfPlanet typeOfPlanet;
+    boolean observable = false;
+    int orbitalPeriod = 0; 
+    int rotationPeriod = 0; 
+
+    public Planet(String name, int numberOfSatellites, double mass, double volume, int diameter, int distanceToTheSun,
+            TypeOfPlanet typeOfPlanet, boolean observable, int orbitalPeriod, int rotationPeriod) {
+        this.name = name;
+        this.numberOfSatellites = numberOfSatellites;
+        this.mass = mass;
+        this.volume = volume;
+        this.diameter = diameter;
+        this.distanceToTheSun = distanceToTheSun;
+        this.typeOfPlanet = typeOfPlanet;
+        this.observable = observable;
+        this.orbitalPeriod = orbitalPeriod; 
+        this.rotationPeriod = rotationPeriod; 
+    }
+
+    public int getOrbitalPeriod() {
+        return orbitalPeriod;
+    }
+
+    public int getRotationPeriod() {
+        return rotationPeriod;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfSatellites() {
+        return numberOfSatellites;
+    }
+
+    public double getMass() {
+        return mass;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public int getDiameter() {
+        return diameter;
+    }
+
+    public int getDistanceToTheSun() {
+        return distanceToTheSun;
+    }
+
+    public TypeOfPlanet getTypeOfPlanet() {
+        return typeOfPlanet;
+    }
+
+    public boolean isObservable() {
+        return observable;
+    }
+
+    public double calculateDensity() {
+        double density = mass / volume;
+        return density;
+    }
+
+    public String isOutside() {
+        if (distanceToTheSun > 314155527 || distanceToTheSun > 508632758) {
+            return "El planeta es exterior";
+        } else {
+            return "El planeta es interior";
+        }
+    }
+
+    
 }
--- src/main/java/dev/personal/java/sistema/solar/model/TypeOfPlanet.java
+++ src/main/java/dev/personal/java/sistema/solar/model/TypeOfPlanet.java
@@ -2,4 +2,5 @@ package dev.personal.java.sistema.solar.model;
 
 public enum TypeOfPlanet {
 
+    GASEOSO, TERRESTRE, ENANO;
 }
