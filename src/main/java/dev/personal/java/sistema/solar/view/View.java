e7d1afb933d1456568883a429f3085b304dea074
jose30-back <josema.bootcamp@gmail.com>
2024-11-25 20:09:47 +0100

    added view complete

--- src/main/java/dev/personal/java/sistema/solar/view/View.java
+++ src/main/java/dev/personal/java/sistema/solar/view/View.java
@@ -1,5 +1,31 @@
 package dev.personal.java.sistema.solar.view;
 
+import dev.personal.java.sistema.solar.model.Planet;
+
 public class View {
 
+    Planet planet; 
+
+    public void printPlanet (){
+        System.out.println("-------Planet-------");
+        System.out.println("Nombre:" + planet.getName());
+        System.out.println("Numero de satelites: " + planet.getNumberOfSatellites());
+        System.out.println("Masa: " + planet.getMass());
+        System.out.println("Volumen: " + planet.getVolume());
+        System.out.println("Diametro: " + planet.getDiameter());
+        System.out.println("Distancia al sol: " + planet.getDistanceToTheSun());
+        System.out.println("Tipo de planeta: " + planet.getTypeOfPlanet());
+        System.out.println("Observable?: " + planet.isObservable());
+        System.out.println("Periodo orbital: " + planet.getOrbitalPeriod());
+        System.out.println("Periodo de rotacion: " + planet.getRotationPeriod());
+    }
+
+    public void printDensity (){ 
+        System.out.println("Densidad: " + planet.calculateDensity());
+    }
+
+    public void printIfObservable () {
+        System.out.println("El planeta es: " + planet.isOutside());
+    }
 }
+
